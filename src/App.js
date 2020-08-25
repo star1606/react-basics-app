0692210a683c65c9cd0361a246e66ba4fa3757e1
Lee <lee@nate.com>
2020-08-25 23:45:56 +0900

    react basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Ninjas from "./Ninjas";
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: "RYU", age: 30, belt: "black", id: 1 },
+      { name: "LEE", age: 27, belt: "white", id: 2 },
+      { name: "Park", age: 22, belt: "orange", id: 3 },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Basics</h1>
+        <p> Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Ninjas.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+const Ninjas = ({ ninjas }) => {
+  // array를 만든다
+  //   const ninjaList = ninjas.map((ninja) => {
+  //     if (ninja.age >= 30) {
+  //       return (
+  //         <div className="ninja" key={ninja.id}>
+  //           <div>name : {ninja.name}</div>
+  //           <div>age : {ninja.age}</div>
+  //           <div>belt : {ninja.belt}</div>
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+
+  const ninjaList = ninjas.map((ninja) => {
+    return ninja.age >= 30 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>name : {ninja.name}</div>
+        <div>age : {ninja.age}</div>
+        <div>belt : {ninja.belt}</div>
+      </div>
+    ) : null;
+  });
+
+  return <div className="ninja-list">{ninjaList}</div>;
+};
+
+export default Ninjas;
